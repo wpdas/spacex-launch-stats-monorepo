970227aed2238b1437601666db6f4d243279ca9f
Wenderson Pires <wendersonpdas@gmail.com>
2020-05-28 03:50:39 -0300

    lint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,11 +3,9 @@ import React, { useEffect } from 'react';
 function App() {
   useEffect(() => {
     const API = 'http://localhost:5000';
-    console.log('Look at this dude:', API);
     const fetchData = async () => {
       const data = await fetch(API);
       const content = await data.text();
-      console.log(content);
     };
 
     fetchData();
